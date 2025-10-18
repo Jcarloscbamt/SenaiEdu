badd07be6b23ce88252316c35d1232d050b430d1
Nathan Botelho Da Silva Campos - FATEC - Cuiaba/MT <nathan.botelho@alunosenai.mt>
2025-10-18 10:38:17 -0400

    criando coordenação

---
+++ app/Livewire/Coordenação.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Coordenação extends Component
+{
+    public function render()
+    {
+        return view('livewire.coordenação');
+    }
+}
