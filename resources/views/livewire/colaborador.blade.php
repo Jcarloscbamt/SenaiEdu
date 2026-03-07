e6501097f8a117fcbcb2b6b6e00bc0962fbe1882
José Carlos <jcarloscba@hotmail.com>
2026-03-07 08:30:23 -0400

    mudando placeholder

--- resources/views/livewire/colaborador.blade.php
+++ resources/views/livewire/colaborador.blade.php
@@ -24,7 +24,7 @@
             <input type="email"
                    wire:model="email"
                    class="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 @error('email') border-red-500 @enderror"
-                   placeholder="Email">
+                   placeholder="Emailss">
             @error('email') 
                 <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
             @enderror
